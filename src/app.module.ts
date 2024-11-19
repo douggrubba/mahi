5230907b4c4945f591c20dbc32ef6c8dbd204808
Doug Grubba <douglas.grubba@gmail.com>
2024-11-18 22:44:51 -0500

    wip

---
+++ src/app.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'mahi.db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+export class AppModule {
+  constructor(private dataSource: DataSource) {
+    console.log('Data source:', this.dataSource);
+  }
+}
+
---
+++ src/auth/constants.ts
@@ -0,0 +1,4 @@
+export const jwtConstants = {
+  // TODO: put into an env
+  secret: 'secretKeyChangeMe'
+};
