14739463a7df9af71abdc36d287d0b6b9df8c826
Doug Grubba <douglas.grubba@gmail.com>
2024-11-25 23:13:45 -0500

    wip

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,23 @@
 import { Get, Controller, Render } from '@nestjs/common';
+import { join } from 'path';
+import * as fs from 'fs';
+import * as Handlebars from 'handlebars';
 
 @Controller()
 export class AppController {
   @Get()
   @Render('index')
   root() {
-    return { message: 'Welcome to Mahi' };
+    // TODO: put this in a helper or a service in nestjs
+    const headerTemplatePath = join(__dirname, '../views/header.hbs')
+    const headerTemplateContent = fs.readFileSync(headerTemplatePath, 'utf8')
+
+    const compiledHeader = Handlebars.compile(headerTemplateContent);
+    const header = compiledHeader({ title: 'My Header Title' });
+
+    return {
+      header,
+      message: 'Welcome to Mahi'
+    };
   }
 }
--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -5,9 +5,13 @@ import {
   HttpCode,
   HttpStatus,
   Post,
+  Render,
   Request,
   UseGuards
 } from '@nestjs/common';
+import { join } from 'path';
+import * as fs from 'fs';
+import * as Handlebars from 'handlebars';
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
@@ -17,8 +21,18 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Get('login')
+  @Render('public')
   signInForm(@Request() req) {
-    return 'login form'
+    // TODO: put this in a helper or a service in nestjs
+    const loginTemplatePath = join(__dirname, '../../views/login.hbs');
+    const publicTemplateContent = fs.readFileSync(loginTemplatePath, 'utf8')
+
+    const compiledLogin = Handlebars.compile(publicTemplateContent);
+    const content = compiledLogin({ title: 'Login' });
+
+    return {
+      content
+    };
   }
 
   @HttpCode(HttpStatus.OK)
